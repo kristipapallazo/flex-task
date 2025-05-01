e41da9fa65befbe4838e64e1a013f1e445f3aa7f
kristipapallazo <papallazokristi@gmail.com>
2025-05-01 22:12:06 +0200

    .

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Layout } from "../node_modules/antd/es/index";
+import "./App.css";
+import "./UI/AntD/GlobalAntD.css";
+
+function App() {
+  return <Layout></Layout>;
+}
+
+export default App;
---
+++ src/UI/AntD/Button/MainBttn.tsx
@@ -0,0 +1,7 @@
+import { Button } from "../../../../node_modules/antd/es/index";
+
+const MainBttn = () => {
+  return <Button>MainBttn</Button>;
+};
+
+export default MainBttn;
